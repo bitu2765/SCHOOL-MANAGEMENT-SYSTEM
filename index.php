8e3b3a0cafeca57bd82a29208be36de86ee8a105
bitu2765 <bhautiksudani16962765@gmail.com>
2021-04-13 13:36:35 +0530

    commit5

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title> Login </title>
     <style>
-        body {
+        /* body {
             background-image: url(images/bg-dark.png);
-        }
+        } */
 
-        a {
+        /* a {
             color: #1f1f1f;
             text-decoration: none;
             outline: none;
-        }
+        } */
 
-        a:hover {
+        /* a:hover {
             color: white;
-        }
+        } */
 
         .login {
             width: 35%;
@@ -48,13 +48,13 @@
             text-align: center;
         }
 
-        .input-field {
+        /* .input-field {
             width: 100%;
             padding: 10px;
             outline: none;
-        }
+        } */
 
-        .btns {
+        /* .btns {
             background-color: #4c5a7d;
             color: white;
             border: none;
@@ -62,7 +62,7 @@
             width: 100%;
             opacity: 0.9;
             height: 40px;
-        }
+        } */
 
         .bg-light {
             background: #4c5a7d !important;
@@ -77,7 +77,7 @@
                 <a class="navbar-brand" href="index.php">
                     <img src="logo.png" alt="SZ WAGHELA HIGH SCHOOL" />
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -95,11 +95,11 @@
                             </div>
                         </li>
                     </ul>
-                </div>
+                </div> -->
             </div>
         </nav>
     </header>
-    <span id="pstd" ></span>
+    <span id="pstd"></span>
     <center>
         <div class="login">
             <h1>LOGIN</h1>
@@ -118,23 +118,18 @@
             </div>
             <!-- </form> -->
         </div>
-        <div class="login" style="margin: 10px; padding: 10px;">
-            <p>Can't Access Account? <a href="#" style="font-weight:bold;color: #4c5a7d;">Reset Password</a></p>
-        </div>
-        <div class="login" style="margin: 10px; padding: 10px;">
-            <p>Don't have Account? <a href="#" style="font-weight:bold;color: #4c5a7d;">Create Now</a></p>
-        </div>
     </center>
-</body>
-<script src="log.js"></script>
-<script>
-    function checkData() {
-        if (document.getElementById('admin_pass').value == "" || document.getElementById('username').value == "") {
-            alert("Please enter username and password!");
-            return false;
+
+    <script src="log.js"></script>
+    <script>
+        function checkData() {
+            if (document.getElementById('admin_pass').value == "" || document.getElementById('username').value == "") {
+                alert("Please enter username and password!");
+                return false;
+            }
+            return true;
         }
-        return true;
-    }
-</script>
+    </script>
+</body>
 
 </html>
