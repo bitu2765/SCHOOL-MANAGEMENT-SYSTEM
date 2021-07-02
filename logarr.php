efcbf13abc9468da9a703403218ea9ac3405538c
bitu2765 <bhautiksudani16962765@gmail.com>
2021-07-02 11:31:26 +0530

    commit6

--- logarr.php
+++ logarr.php
@@ -1,20 +1,20 @@
 <?php
 
-$con = mysqli_connect("localhost",$_POST["us"],$_POST["ps"],"schoolmanage");
+// $con = mysqli_connect("localhost",$_POST["us"],$_POST["ps"],"schoolmanage");
 // echo "done";
-    if(mysqli_connect_error())
-    {
-        // echo "<script>alert('Admin login not successfull');
-        //                   window.location.href = 'login.php';
-        //         </script>";
-    }
-    else
+    // if(mysqli_connect_error())
+    // {
+    //     // echo "<script>alert('Admin login not successfull');
+    //     //                   window.location.href = 'login.php';
+    //     //         </script>";
+    // }
+    if(md5("szvaghelaadmin")==md5($_POST["us"]) || md5("pass")==md5($_POST["ps"]))
     {
         echo "success";
 
         session_start();
-        $_SESSION["user"]=$_POST["us"];
-        $_SESSION["pass"]=$_POST["ps"];
+        $_SESSION["user"]="admin";
+        $_SESSION["pass"]="admin";
         // echo "<script>alert('Admin login successfull');
         //                   window.location.href = 'Dashboard.php';
         //         </script>";
