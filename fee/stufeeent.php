49ce3334bab8cb9a308add22c44aad86d380a80e
bitu2765 <bhautiksudani16962765@gmail.com>
2021-07-02 09:58:07 +0530

    commit6

--- fee/stufeeent.php
+++ fee/stufeeent.php
@@ -4,14 +4,21 @@ $json = $_POST["sfd"];
 $sd = json_decode($json);
 $stuar = $sd->stu;
 $fepmo = intval($sd->fee);
+$feesofmonth = intval($fepmo/12);
 $tt = 0;
 
 for ($i = 0; $i < count($stuar); $i++) {
+    $tecount=0;
     $fepa = 0;
     for ($j = 1; $j <= 12; $j++) {
         if ($stuar[$i][$j]) {
-            $fepa += ($fepmo/12);
+            $fepa +=$feesofmonth;
         }
+        $tecount++;     
+    }
+    if($tecount ==12)
+    {
+        $fepa = $fepmo;
     }
     $query = "UPDATE tbfeesmaster SET FeesJanuary='" . $stuar[$i][1] . "',`FeesFebruary`='" . $stuar[$i][2] . "',`FeesMarch`='" . $stuar[$i][3] . "',`FeesApril`='" . $stuar[$i][4] . "',`FeesMay`='" . $stuar[$i][5] . "',`FeesJune`='" . $stuar[$i][6] . "',`FeesJuly`='" . $stuar[$i][7] . "',`FeesAugust`='" . $stuar[$i][8] . "',`FeesSeptember`='" . $stuar[$i][9] . "',`FeesOctomber`='" . $stuar[$i][10] . "',`FeesNovember`='" . $stuar[$i][11] . "',`FeesDecember`='" . $stuar[$i][12] . "',FeesPaid='" . $fepa . "',FeesDue='" . $fepmo - $fepa . "' WHERE FeesAcademicYear='" . $sd->ay . "' AND FeesStandard='" . $sd->std . "' AND FeesDivison='" . $sd->div . "' AND FeesStudentRollNo='" . $stuar[$i][0] . "'; ";
     if ($con->query($query)) {
