c2daf004b457d41739847e480f4e9741731ef97e
bitu2765 <bhautiksudani16962765@gmail.com>
2021-07-03 17:30:08 +0530

    commit6

--- fee/stufeeent.php
+++ fee/stufeeent.php
@@ -13,13 +13,14 @@ for ($i = 0; $i < count($stuar); $i++) {
     for ($j = 1; $j <= 12; $j++) {
         if ($stuar[$i][$j]) {
             $fepa += $feesofmonth;
+            $tecount++;
         }
-        $tecount++;
     }
     if ($tecount == 12) {
         $fepa = $fepmo;
     }
-    $query = "UPDATE tbfeesmaster SET FeesJanuary='" . intval($stuar[$i][1]) . "',FeesFebruary='" . intval($stuar[$i][2]) . "',FeesMarch='" . intval($stuar[$i][3]) . "',FeesApril='" . intval($stuar[$i][4]) . "',FeesMay='" . intval($stuar[$i][5]) . "',FeesJune='" . intval($stuar[$i][6]) . "',FeesJuly='" . intval($stuar[$i][7]) . "',FeesAugust='" . intval($stuar[$i][8]) . "',FeesSeptember='" . intval($stuar[$i][9]) . "',FeesOctomber='" . intval($stuar[$i][10]) . "',FeesNovember='" . intval($stuar[$i][11]) . "',FeesDecember='" . intval($stuar[$i][12]) . "',FeesPaid='" . $fepa . "',FeesDue='" . $fepmo - $fepa . "' WHERE FeesAcademicYear='" . $sd->ay . "' AND FeesStandard='" . intval($sd->std) . "' AND FeesDivison='" . $sd->div . "' AND FeesStudentRollNo='" . intval($stuar[$i][0]) . "'; ";
+    $fedue = $fepmo - $fepa;
+    $query = "UPDATE tbfeesmaster SET FeesJanuary='" . $stuar[$i][1] . "',FeesFebruary='" . $stuar[$i][2] . "',FeesMarch='" . $stuar[$i][3] . "',FeesApril='" . $stuar[$i][4] . "',FeesMay='" . $stuar[$i][5] . "',FeesJune='" . $stuar[$i][6] . "',FeesJuly='" . $stuar[$i][7] . "',FeesAugust='" . $stuar[$i][8] . "',FeesSeptember='" . $stuar[$i][9] . "',FeesOctomber='" . $stuar[$i][10] . "',FeesNovember='" . $stuar[$i][11] . "',FeesDecember='" . $stuar[$i][12] . "',FeesPaid='" . $fepa . "',FeesDue='" . $fedue . "' WHERE FeesAcademicYear='" . $sd->ay . "' AND FeesStandard='" . $sd->std . "' AND FeesDivison='" . $sd->div . "' AND FeesStudentRollNo='" . $stuar[$i][0] . "'; ";
     if ($con->query($query)) {
         $tt++;
     }
@@ -29,3 +30,5 @@ if (count($stuar) == $tt) {
 } else {
     echo "<div class='alert alert-danger alert-dismissible' role='alert'><button type='button' class='btn-close' data-dismiss='alert' aria-label='Close'></button><div class='alert-message'>Fees Details Not Updated Successfully!</div></div>";
 }
+
+
